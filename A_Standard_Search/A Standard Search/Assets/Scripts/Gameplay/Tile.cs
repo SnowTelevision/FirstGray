4eb82e0b152b1ddbeec2b2528dfc60e522f4584a
zym625 <522367299@qq.com>
2021-01-29 21:05:11 -0800

    add     public bool isExit;

--- A_Standard_Search/A Standard Search/Assets/Scripts/Gameplay/Tile.cs
+++ A_Standard_Search/A Standard Search/Assets/Scripts/Gameplay/Tile.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
 
+
+
     public int currState;
     private Animator animator;
-    private enum animatenames {a1,a2,a3,a4};
+    public bool isExit;
+
+    void Start()
+    {
+       animator= GetComponent<Animator>();
+    }
 
 
     public void StateChange(int state)
     {
         if(state == 0 && currState == 0)//light to light
         {
-            playAnime(0);
+            animator.playAnime(0);
         }
         if (state == 0 && currState == 1)//light to dark
         {
@@ -40,10 +47,6 @@ public class Tile : MonoBehaviour
         return;
     } 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
