78883bce69af4c125dccb37d011d9dc60e175582
zym625 <522367299@qq.com>
2021-01-29 19:56:19 -0800

    Update Tile.cs

--- A_Standard_Search/A Standard Search/Assets/Scripts/Gameplay/Tile.cs
+++ A_Standard_Search/A Standard Search/Assets/Scripts/Gameplay/Tile.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class NewBehaviourScript : MonoBehaviour
+public class Tile : MonoBehaviour
 {
 
     public int currState;
     public int animate;
+    private Animator animator;
+    private enum animatenames {a1,a2,a3,a4};
 
 
-    void StateChange(int state)
+    public void StateChange(int state)
     {
+        if(state == 0 && currState == 0)//light to light
+        {
+            playAnime(0);
+        }
+        if (state == 0 && currState == 1)//light to dark
+        {
+            playAnime(0);
+        }
+        if (state == 1 && currState == 0)//dark to light
+        {
+            playAnime(0);
+        }
+        if (state == 1 && currState == 1)//dark to dark
+        {
+            playAnime(0);
+        }
         Debug.Log("My state changes.");
+        currState = state;
         return;
     }
+
+    void playAnime(int animeID)//play a state change animate 
+    {
+
+        return;
+    } 
     // Start is called before the first frame update
     void Start()
     {
