48d3acf9dffdb750ce64f32e21ad579d37b6c3b7
zym625 <522367299@qq.com>
2021-01-29 19:30:26 -0800

    +Tile Control Script

---
+++ A_Standard_Search/A Standard Search/Assets/Scripts/Gameplay/Tile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public int currState;
+    public int animate;
+
+
+    void StateChange(int state)
+    {
+        Debug.Log("My state changes.");
+        return;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
